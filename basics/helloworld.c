09b7fa937896a444b0b8419d80308f4b97885dc2
aswini7 <madireddyaswini@gmail.com>
2018-08-31 16:42:34 +0530

    hello.c commit

---
+++ basics/helloworld.c
@@ -0,0 +1,10 @@
+ Volume in drive F is New Volume
+ Volume Serial Number is 828E-547B
+
+ Directory of F:\labs\basics
+
+31-08-2018  16:05    <DIR>          .
+31-08-2018  16:05    <DIR>          ..
+31-08-2018  16:05                 0 helloworld.c
+               1 File(s)              0 bytes
+               2 Dir(s)  249,173,524,480 bytes free
